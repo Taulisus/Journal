6ac52d084f472cf1b46dde623e87fde85e264248
Taulisus <333kekca@gmail.com>
2026-09-21 14:20:10 +0400

    test 1

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    # SESSION_COOKIE_SECURE = True  # Раскомментируйте, если приложение работает только по HTTPS
+
     DATABASE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'journal.db')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
